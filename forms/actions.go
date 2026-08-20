7f21323fab0a313021876f8378832a6f5b23abfc
stern <ds@zettelstore.de>
2026-08-20 10:15:00 +0000

    feat(forms): add TrimAction, to trim field values before validation.
    FossilOrigin-Name: 3cf9f59bae06e167c118a7ae864486d0b5a828b39f5b8aff93d538fe332e14f3

---
+++ forms/actions.go
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2026-present Detlef Stern
+//
+// This file is part of sxwebs.
+//
+// sxwebs is licensed under the latest version of the EUPL // (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
+//
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2026-present Detlef Stern
+// -----------------------------------------------------------------------------
+
+package forms
+
+import "strings"
+
+// Actions are technically validators. They are used to modify field values.
+
+// TrimAction removes all leading and trailing white space.
+type TrimAction struct{}
+
+// Check will trim the given field.
+func (TrimAction) Check(_ *Form, fld Field) error {
+	return fld.SetValue(strings.TrimSpace(fld.Value()))
+}
---
+++ forms/actions_test.go
@@ -0,0 +1,47 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2026-present Detlef Stern
+//
+// This file is part of sxwebs.
+//
+// sxwebs is licensed under the latest version of the EUPL // (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
+//
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2026-present Detlef Stern
+// -----------------------------------------------------------------------------
+
+package forms_test
+
+import (
+	"testing"
+
+	"t73f.de/r/webs/forms"
+)
+
+func TestTrimAction(t *testing.T) {
+	fld := forms.TextAreaField("name", "label")
+	if err := fld.SetValue("\tbla  fasel\n "); err != nil {
+		t.Error("SetValue", err)
+		return
+	}
+	trim := forms.TrimAction{}
+	if err := trim.Check(nil, fld); err != nil {
+		t.Error("Check", err)
+		return
+	}
+	exp := "bla  fasel"
+	if got := fld.Value(); exp != got {
+		t.Errorf("expected %q, but got %q", exp, got)
+	}
+
+	fld = forms.TextAreaField("name", "label", trim, forms.StringEqual("x", "msg"))
+	if err := fld.SetValue("\t x \n "); err != nil {
+		t.Error("SetValue2", err)
+		return
+	}
+	form := forms.Define(fld)
+	if !form.IsValid() {
+		t.Error("IsValid()", form.Messages())
+	}
+}
